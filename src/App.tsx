88513d1f2c00e9afff1c77a13e480af10cea2421
Nguyen <nguyenne132@gmail.com>
2022-02-17 01:38:15 +0700

    setting router

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,27 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
 import postApi from './api/postApi';
 import todoApi from './api/todoApi';
+import Login from './features/auth/pages/Login';
+import { Route, Routes } from 'react-router-dom';
+import Admin from './components/layout/Admin';
+import { NotFound } from './components/common';
 
 function App() {
 
-  useEffect(()=>{
-    // postApi.getAll().then(res=>console.log(res))
-    todoApi.getById("1").then(res=>console.log(res))
-  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <div >
+      <Routes>
+        <Route path='/login' element={<Login/>} ></Route>
+
+        <Route path='/admin'  element={<Admin/>}></Route>
+        <Route path="" element={<NotFound/>}></Route>
+
+      </Routes> 
+      <div>helo</div>
+         </div>
+
   );
 }
 
---
+++ src/components/common/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export  function NotFound({}: Props) {
+  return (
+    <div>NotFound</div>
+  )
+}
